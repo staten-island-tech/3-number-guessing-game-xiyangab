9685e566e8719804b2165ef24c49280c6590d366
xiyangab <xiyangh@nycstudents.net>
2025-09-30 05:58:47 -0700

    finished number gme

--- numberproject.py
+++ numberproject.py
@@ -1,13 +1,12 @@
 import random
 
 random_integer = random.randint(1,10)
+print(random_integer)
 
 guess = int(input("guess a number 1-10"))
-guess_count = 0
 guess_history = []
 
 while guess != random_integer:
-    guess_count = guess_count + 1
     if guess > random_integer:
         print("your guess is greater")
         guess_history.append(guess)
@@ -21,11 +20,6 @@ while guess != random_integer:
 
 if guess == random_integer:
     print("woohoo you guessed the number!!")
-    guess_count = guess_count + 1
-
-if guess_count == 1:
-    print(f"you guessed the number first try!!")
-    print (f"your guesses: {guess_history}")
-else:
-    print(f"it took you {guess_count} tries to get the number!!")
-    print (f"your guesses: {guess_history}")
+    guess_history.append(guess)
+    print(f"you guessed {len(guess_history)} times")
+    print(f"your guesses were: {guess_history}")
